020a68bcccf891ce0fba698f0208178256ff2563
Basel Qarqa <basel.m.qarqa@gmail.com>
2023-06-21 22:42:17 +0300

    update portfolio

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,45 @@
-const hamburger =document.querySelector(".hamburger");
-const navMenu =document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const darkMode = document.getElementById("dark-mode");
+const darkModeMobile = document.getElementById("dark-mode-mobile");
+const body = document.body;
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-hamburger.addEventListener("click",()=>{
-    hamburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
-})
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
 
-document.querySelectorAll(".nav-link").forEach(n =>n.addEventListener("click",()=>{
-    hamburger.classList.remove("active")
-    navMenu.classList.remove("active")
-}))
+// ----------------------*** Dark Mode ***----------------------
+darkMode.addEventListener("click", function () {
+  body.classList.toggle("dark-mode");
+  setThemeInLocalStorage();
+});
+darkModeMobile.addEventListener("click", function () {
+  body.classList.toggle("dark-mode");
+  setThemeInLocalStorage();
+});
+
+function setThemeInLocalStorage() {
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("dark-mode", "dark");
+  } else {
+    localStorage.setItem("dark-mode", "light");
+  }
+}
+
+window.addEventListener("load", function () {
+  if (localStorage.length == 0) {
+    localStorage.setItem("dark-mode", "light");
+  } else {
+    const value = localStorage.getItem("dark-mode");
+    value == "dark"
+      ? body.classList.add("dark-mode")
+      : body.classList.remove("dark-mode");
+  }
+});
